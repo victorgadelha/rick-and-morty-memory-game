f0fe950ecce893785bbeba336824e2825abb1230
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2022-08-14 20:03:06 -0300

    feat: passando todos os arquivos

---
+++ js/game.js
@@ -0,0 +1,13 @@
+const creatCard = () => {
+
+    const card = document.createElement('div')
+    const front = document.createElement('div')
+    const back = document.createElement('div')
+
+    card.className = 'card'
+    front.className = 'face front'
+    back.className = 'face back'
+
+    card.appendChild(front)
+    card.appendChild(back)
+}
