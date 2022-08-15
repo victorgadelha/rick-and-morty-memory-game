6260934c3aed6134c08a4a8bbc7a7ebb0d12ea37
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2022-08-14 21:30:27 -0300

    feat: criando aleatoriedade das cartas

--- js/game.js
+++ js/game.js
@@ -18,11 +18,13 @@ const createElement = (tag, className) => {
     return element
 }
 
-const creatCard = () => {
+const creatCard = (character) => {
 
     const card = createElement('div', 'card')
     const front = createElement('div', 'face front')
     const back = createElement('div', 'face back')
+
+    front.style.backgroundImage = `url('../images/${character}.png')`
 
     card.appendChild(front)
     card.appendChild(back)
@@ -31,11 +33,13 @@ const creatCard = () => {
 }
 
 const loadGame = () => {
+    const duplicateCharacters = [ ...characters, ...characters]
 
-    characters.forEach((character) => {
-        const card = creatCard()
+    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5 )
+
+    shuffledArray.forEach((character) => {
+        const card = creatCard(character)
         grid.appendChild(card)
-
     })
 
     
